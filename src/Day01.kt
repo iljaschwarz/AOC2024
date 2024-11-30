be5335532fd7e2506a071123af82a941eadc0c76
Ilja Schwarz <iljaschwarz@gmail.com>
2024-11-30 20:41:01 +0100

    batman

---
+++ src/Day01.kt
@@ -0,0 +1,15 @@
+fun main() {
+    fun part1(input: List<String>): Any {
+        return input
+    }
+
+    fun part2(input: List<String>): Any {
+        return input
+    }
+
+
+    val input = readInput("Day01")
+    // val input = listOf()
+    part1(input).println()
+    part2(input).println()
+}

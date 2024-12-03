ac4628e00070d2b46bc64e4c65c8fbfee18c24e7
Ilja Schwarz <iljaschwarz@gmail.com>
2024-12-03 21:10:02 +0100

    day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,53 @@
 fun main() {
     fun part1(input: List<String>): Any {
-        return input
+        val left = ArrayList<Int>()
+        val right = ArrayList<Int>()
+        var sum = 0
+
+        input.forEach {
+            val line = it.split("   ")
+            left.add(line[0].toInt())
+            right.add(line[1].toInt())
+        }
+        left.sort()
+        right.sort()
+        for (i in 0..<left.size) {
+            sum += if (left[i] > right[i])
+                left[i] - right[i]
+            else
+                right[i] - left[i]
+        }
+        return sum
     }
 
     fun part2(input: List<String>): Any {
-        return input
+        var sum = 0
+        val left = ArrayList<Int>()
+        val right = HashMap<Int, Int>()
+        input.forEach {
+            val line = it.split("   ")
+            left.add(line[0].toInt())
+            val i = right.getOrElse(line[1].toInt()) { 0 }
+            right[line[1].toInt()] = i + 1
+
+        }
+        left.forEach {
+            if (right.contains(it)) {
+                sum += it * right[it]!!
+            }
+        }
+
+        return sum
     }
 
 
     val input = readInput("Day01")
-    // val input = listOf()
+    //val input = listOf("3   4" ,
+    //         "4   3",
+    //        "2   5" ,
+    //         "1   3" ,
+    //          "3   9" ,
+    //          "3   3")
     part1(input).println()
     part2(input).println()
 }
